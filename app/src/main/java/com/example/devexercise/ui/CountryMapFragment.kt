8c44938aff81896bb1ba6554813efd68745e9c20
Juan Mantilla <juan.mantilla.brito@udla.edu.ec>
2020-09-07 19:38:43 -0400

    Offline mode - Code cleanup

--- app/src/main/java/com/example/devexercise/ui/CountryMapFragment.kt
+++ app/src/main/java/com/example/devexercise/ui/CountryMapFragment.kt
@@ -1,7 +1,7 @@
 package com.example.devexercise.ui
 
 import android.app.Dialog
-import android.content.Context
+import android.app.ProgressDialog
 import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
@@ -53,61 +53,104 @@ class CountryMapFragment : Fragment(), Injectable {
 
         viewModelAdapter = MapPointAdapter()
 
+        val downloadDialog = createProgressDialog()
+
         val country = CountryMapFragmentArgs.fromBundle(requireArguments()).selectedCountry
 
         downloadArea = Graphic()
         downloadArea?.symbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLACK, 2f)
         graphicsOverlay.graphics.add(downloadArea)
 
-        binding.mapView.let {
-            it.map = viewModel.createMapCountry(country)
-            it.graphicsOverlays.add(graphicsOverlay)
-            it.onTouchListener = object : DefaultMapViewOnTouchListener(context, it) {
-                override fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
+        if(viewModel.tiledMap == null){
+            binding.mapView.visibility = View.INVISIBLE
+            binding.mapMessage.visibility = View.VISIBLE
+            binding.updateMap.visibility = View.INVISIBLE
+            binding.downloadMap.visibility = View.INVISIBLE
+        }else{
+            binding.mapView.visibility = View.VISIBLE
+            binding.mapMessage.visibility = View.INVISIBLE
+            binding.updateMap.visibility = View.VISIBLE
+            binding.downloadMap.visibility = View.VISIBLE
 
-                    val tappedPoint = it.screenToLocation(android.graphics.Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt()))
-                    val tolerance = 25
-                    val mapTolerance = tolerance * it.unitsPerDensityIndependentPixel
-                    val envelope = Envelope(tappedPoint.x - mapTolerance, tappedPoint.y - mapTolerance, tappedPoint.x + mapTolerance, tappedPoint.y + mapTolerance, it.spatialReference)
-                    val pointSelectedOnMap = viewModel.getPointOnMap(envelope)
+            binding.mapView.let {
+                it.map = viewModel.createMapCountry(country)
+                it.graphicsOverlays.add(graphicsOverlay)
+                it.onTouchListener = object : DefaultMapViewOnTouchListener(context, it) {
+                    override fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
 
-                    pointSelectedOnMap.addDoneListener {
-                        try {
-                            val pointRequested = viewModel.getMapPointInfo(pointSelectedOnMap.get())
-                            pointRequested.observe(viewLifecycleOwner, Observer { point ->
-                                point?.apply {
-                                    viewModelAdapter?.pointDetails = point
-                                    //println(point)
-                                }
-                            })
-                            showPointDetails()
-                        } catch (e: Exception) {
-                            Snackbar.make(activity!!.findViewById(android.R.id.content), "Point selected failed: " + e.message, Snackbar.LENGTH_LONG).show()
+                        val tappedPoint = it.screenToLocation(Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt()))
+                        val tolerance = 25
+                        val mapTolerance = tolerance * it.unitsPerDensityIndependentPixel
+                        val envelope = Envelope(tappedPoint.x - mapTolerance, tappedPoint.y - mapTolerance, tappedPoint.x + mapTolerance, tappedPoint.y + mapTolerance, it.spatialReference)
+                        val pointSelectedOnMap = viewModel.getPointOnMap(envelope)
+
+                        pointSelectedOnMap.addDoneListener {
+                            try {
+                                val pointRequested = viewModel.getMapPointInfo(pointSelectedOnMap.get())
+                                pointRequested.observe(viewLifecycleOwner, Observer { point ->
+                                    point?.apply {
+                                        viewModelAdapter?.pointDetails = point
+                                        //println(point)
+                                    }
+                                })
+                                showPointDetails()
+                            } catch (e: Exception) {
+                                Snackbar.make(activity!!.findViewById(android.R.id.content), "Point selected failed: " + e.message, Snackbar.LENGTH_LONG).show()
+                            }
                         }
+
+                        return super.onSingleTapConfirmed(motionEvent)
                     }
+                }
 
-                    return super.onSingleTapConfirmed(motionEvent)
+                it.addViewpointChangedListener {
+                    val minScreenPoint =  Point(200,200)
+                    val maxScreenPoint = Point(mapView.width - 200, mapView.height - 200)
+                    val minPoint = mapView.screenToLocation(minScreenPoint)
+                    val maxPoint = mapView.screenToLocation(maxScreenPoint)
+                    if(minPoint != null && maxPoint != null){
+                        val envelope = Envelope(minPoint, maxPoint)
+                        downloadArea?.geometry = envelope
+                    }
                 }
             }
-
-            it.addViewpointChangedListener {
-                val minScreenPoint =  Point(200,200)
-                val maxScreenPoint = Point(mapView.width - 200, mapView.height - 200)
-                val minPoint = mapView.screenToLocation(minScreenPoint)
-                val maxPoint = mapView.screenToLocation(maxScreenPoint)
-                if(minPoint != null && maxPoint != null){
-                    val envelope = Envelope(minPoint, maxPoint)
-                    downloadArea?.geometry = envelope
-                    println(envelope)
-                }
-            }
-
-            binding.updateMap.setOnClickListener {
-                viewModel.refreshMap()
-            }
-
-            return binding.root
         }
+
+        binding.updateMap.setOnClickListener {
+            viewModel.refreshMap()
+        }
+
+        binding.downloadMap.setOnClickListener {
+            try{
+                val minScale = binding.mapView.mapScale
+                val maxScale = binding.mapView.map.maxScale
+                viewModel.sendAreaToDownload(downloadArea!!.geometry, minScale, maxScale)
+                viewModel.downloadStatus.observe(viewLifecycleOwner, Observer { downloadStatus ->
+                    when(downloadStatus){
+                        "PREPARED" -> Snackbar.make(requireActivity().findViewById(android.R.id.content), "Preparing download.", Snackbar.LENGTH_LONG).show()
+                        "SUCCEEDED" -> Snackbar.make(requireActivity().findViewById(android.R.id.content), "Area successfully downloaded.", Snackbar.LENGTH_LONG).show()
+                        "FAILED" -> Snackbar.make(requireActivity().findViewById(android.R.id.content), "The area selected exceeds the limits allowed. Please zoom in the area on the map.", Snackbar.LENGTH_LONG).show()
+                    }
+                })
+                viewModel.downloadProgress.observe(viewLifecycleOwner, Observer {
+                    downloadDialog.show()
+                    downloadDialog.progress = it
+                    if(downloadDialog.progress == 100) downloadDialog.dismiss()
+                })
+            }catch(e: Exception){
+                println("Download error, please try again.")
+            }
+        }
+
+        viewModel.isOnline.observe(viewLifecycleOwner, Observer { isOnline ->
+            if(!isOnline){
+                Snackbar.make(requireActivity().findViewById(android.R.id.content), "You are offline now.", Snackbar.LENGTH_LONG).show()
+                binding.updateMap.visibility =  View.INVISIBLE
+                binding.downloadMap.visibility =  View.INVISIBLE
+            }
+        })
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -136,19 +179,33 @@ class CountryMapFragment : Fragment(), Injectable {
         dialog.show()
     }
 
+    private fun createProgressDialog(): ProgressDialog {
+        val progressDialog = ProgressDialog(requireContext())
+        progressDialog.setTitle("Downloading")
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+        progressDialog.isIndeterminate = false
+        progressDialog.progress = 0
+        return progressDialog
+    }
+
     override fun onPause() {
         super.onPause()
-        mapView.pause()
+        if(viewModel.tiledMap != null){
+            mapView.pause()
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        mapView.resume()
+        if(viewModel.tiledMap != null){
+            mapView.resume()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        mapView.dispose()
+        if(viewModel.tiledMap != null){
+            mapView.dispose()
+        }
     }
-
 }
